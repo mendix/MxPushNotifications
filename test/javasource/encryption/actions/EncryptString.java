16f7e4b03e80455f746fe34787ffa059b817f48c
Kevin Vlaanderen <kevin.vlaanderen@mendix.com>
2016-06-27 15:11:28 +0200

    #821257: Add proxies to .gitignore. Remove all existing generated 'proxies' source files

--- test/javasource/encryption/actions/EncryptString.java
+++ test/javasource/encryption/actions/EncryptString.java
@@ -16,9 +16,6 @@ import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.webui.CustomJavaAction;
 import org.apache.commons.codec.binary.Base64;
 
-/**
- * 
- */
 public class EncryptString extends CustomJavaAction<String>
 {
 	private String value;
